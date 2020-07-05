71a14fcaa8b195634c061172e1ee0cbdbbca79b0
Miracle-Messi <wuyf@bupt.edu.cn>
2020-07-05 19:33:32 +0800

    :bulb: Add some comments in source code

--- src/model/friendRequests.js
+++ src/model/friendRequests.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose')
 const db = require('../database')
 
 const FriendRequestsSchema = new mongoose.Schema({
-  fromUserId: { type: String },
-  toUserId: { type: String }
+  fromUserId: { type: String }, // Requester user id
+  toUserId: { type: String } // Receiver user id
 })
 
 const FriendRequestsModel = db.model('friendRequests', FriendRequestsSchema)
